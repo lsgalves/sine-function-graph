2c488b02af742dc7f6e946669d131a9dad97521e
Leonardo Galves <40345300+LeonardoGalves@users.noreply.github.com>
2019-03-07 21:34:36 -0300

    Atualização na identação

--- main.cpp
+++ main.cpp
@@ -13,33 +13,33 @@ int main(int argc, char *argv[]) {
 	lins = 1;
 	cols = 10;
 
-	py = (lins -(-lins))/LINHAS;
-	px = (cols -(-cols))/COLUNAS;
+	py = (lins - (-lins)) 	/ LINHAS;
+	px = (cols - (-cols)) / COLUNAS;
 
 	// CRIANDO PLANO CARTESIANO
-	for(i = 0; i < LINHAS; i++){
-		for(j = 0; j < COLUNAS; j++){
-			if((i == (LINHAS/2)) && (j == (COLUNAS/2))){
+	for (i = 0; i < LINHAS; i++) {
+		for (j = 0; j < COLUNAS; j++){
+			if ((i == (LINHAS / 2)) && (j == (COLUNAS / 2))) {
 				x[i][j] = '+';
-			}else if(i == (LINHAS/2)){
+			} else if (i == (LINHAS/2)) {
 				x[i][j] = '-';
-			}else if(j == (COLUNAS/2)){
+			} else if (j == (COLUNAS/2)) {
 				x[i][j] = '|';
-			}else{
+			} else {
 				x[i][j] = ' ';
 			}
 		}
 	}
 
 	// CRIANDO A FUNCAO SENO
-	for(i = 0; i < COLUNAS; i++){
+	for (i = 0; i < COLUNAS; i++) {
 		seno[i] = sin(v);
-		aux = seno[i]/py;
-		if(aux > 0){
-			aux = LINHAS/2 + aux;
-		}else{
+		aux = seno[i] / py;
+		if (aux > 0) {
+			aux = LINHAS / 2 + aux;
+		} else {
 			aux *= -1;
-			aux = LINHAS/2 - aux;
+			aux = LINHAS / 2 - aux;
 		}
 		x[aux][i] = '+';
 		v += px;
@@ -47,8 +47,8 @@ int main(int argc, char *argv[]) {
 
 	// PRINTANDO A MATRIZ
 	std::cout << std::endl;
-	for(i = 0; i < LINHAS; i++){
-		for(j = 0; j < COLUNAS; j++){
+	for (i = 0; i < LINHAS; i++) {
+		for (j = 0; j < COLUNAS; j++) {
 			std::cout << x[i][j];
 		}
 		std::cout << std::endl;
